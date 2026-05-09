3f926abb9c32927a35394230bcb2cdf7b621f36f
chaewon41 <chaew0m3x@gmail.com>
2026-05-10 02:15:37 +0900

    style: queryClient, axiosInstance 설정 파일 포맷 정리

--- src/shared/api/axios-instance.ts
+++ src/shared/api/axios-instance.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        "Content-Type" : "application/json",
-    },
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type" : "application/json",
+  },
 });

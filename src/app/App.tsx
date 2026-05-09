2c51895bc6180c06b3c74601a5e527070578c521
JS MacBook Pro <j_seokpark@naver.com>
2026-05-10 03:45:20 +0900

    init: route.lazy 활용 라우팅 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router";
+import { router } from "@app/router/router";
+
 function App() {
-  return <>29cm</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;

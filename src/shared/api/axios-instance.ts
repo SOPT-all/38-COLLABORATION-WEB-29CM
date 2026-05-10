f2732cf7323b382e6d6bc473b427cfe27a6130c0
chaewon41 <chaew0m3x@gmail.com>
2026-05-10 15:19:35 +0900

    fix: API 요청 기본 timeout 설정

--- src/shared/api/axios-instance.ts
+++ src/shared/api/axios-instance.ts
@@ -1,10 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
+  timeout: 10000,
   headers: {
-    "Content-Type" : "application/json",
+    'Content-Type': 'application/json',
   },
 });

915d49ee7c8444c62d2f722db42d8af1a66cb82b
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-05-11 11:12:27 +0900

    chore: 예시 stories 파일 추가

---
+++ src/pages/home/components/placeholder.stories.ts
@@ -0,0 +1,11 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+const meta: Meta = {
+  title: 'Placeholder',
+};
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {};

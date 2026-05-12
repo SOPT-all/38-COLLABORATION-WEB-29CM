ac63f8281d16ec0e2224241d5c3fe6d52a4f3f98
chaewon41 <chaew0m3x@gmail.com>
2026-05-13 00:38:48 +0900

    chore: 헤더 상수 분리

---
+++ src/shared/ui/header/constants.ts
@@ -0,0 +1,35 @@
+import {
+  UsercircleIcon,
+  HeartFilledIcon,
+  ShoppingbagIcon,
+  LoginIcon,
+} from '@shared/icons';
+
+export const ACTION_ITEMS = [
+  {
+    label: 'MY PAGE',
+    icon: UsercircleIcon,
+  },
+  {
+    label: 'MY LIKE',
+    icon: HeartFilledIcon,
+  },
+  {
+    label: 'SHOPPING BAG',
+    icon: ShoppingbagIcon,
+  },
+  {
+    label: 'LOGIN',
+    icon: LoginIcon,
+  },
+];
+
+export const NAV_ITEMS = [
+  'Shopping',
+  'Special-Order',
+  'Showcase',
+  'PT',
+  '29Magazine',
+];
+
+export const SUB_NAV_ITEMS = ['Event', 'Lookbook'];

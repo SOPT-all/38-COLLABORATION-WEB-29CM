b2fff0546e5b999788ecbeea63d21537c470e8e1
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-05-12 18:13:14 +0900

    fix: cva 공통 클래스에서 justify-center 제거하여 클래스 충돌 해결

--- src/shared/ui/chip/chip.tsx
+++ src/shared/ui/chip/chip.tsx
@@ -2,11 +2,11 @@ import { ChevronRightSmallIcon } from '@shared/icons';
 import { cva } from 'class-variance-authority';
 
 const chipVariants = cva(
-  'flex items-center justify-center rounded-sm', // 공통 클래스
+  'flex items-center rounded-sm', // 공통 클래스
   {
     variants: {
       variant: {
-        chip: 'bg-gray-200 px-2 py-px text-caption-02-semibold text-gray-800',
+        chip: 'justify-center bg-gray-200 px-2 py-px text-caption-02-semibold text-gray-800',
         cta: 'justify-between bg-black px-3 h-6 text-body-01 text-white gap-2.5',
       },
     },

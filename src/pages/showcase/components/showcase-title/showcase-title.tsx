e5b865cfd98217240422ea3960fa5edcffb99260
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-05-13 01:26:36 +0900

    refactor: 시맨틱 태그로 변경

--- src/pages/showcase/components/showcase-title/showcase-title.tsx
+++ src/pages/showcase/components/showcase-title/showcase-title.tsx
@@ -5,14 +5,14 @@ interface ShowcaseSectionTitleProps {
 
 const ShowcaseSectionTitle = ({ theme, title }: ShowcaseSectionTitleProps) => {
   return (
-    <div className="w-full px-9 py-6">
+    <header className="w-full px-9 py-6">
       <div className="flex h-12 items-center gap-2.5">
-        <div className="text-heading-02 flex h-full items-center bg-gray-900 px-2.5 text-white">
+        <h2 className="text-heading-02 flex h-full items-center bg-gray-900 px-2.5 text-white">
           {theme}
-        </div>
+        </h2>
         <span className="text-title-02">{title}</span>
       </div>
-    </div>
+    </header>
   );
 };
 

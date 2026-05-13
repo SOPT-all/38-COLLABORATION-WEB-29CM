ffb12b7895581c6b9a61af2916b15ebe9bf12e0c
pwc2002 <pwc2002@naver.com>
2026-05-14 01:43:45 +0900

    refactor: api 명세에 맞추어, interface 수정, 좋아요 상태 추가

--- src/pages/home/components/home-product/home-product-list.tsx
+++ src/pages/home/components/home-product/home-product-list.tsx
@@ -1,41 +1,63 @@
+import { HeartBlankedIcon, HeartFilledIcon } from '@shared/icons';
 import Chip from '@shared/ui/chip/chip';
 import Image from '@shared/ui/image';
 
 interface HomeProductListProps {
-  brand: string;
-  title: string;
-  price: number;
-  discountRate: number;
-  badges: string[];
+  productId: number;
   imageUrl: string;
+  brandName: string;
+  name: string;
+  saleRate: number;
+  price: number;
+  tags: string[];
+  likeCount: number;
+  isLiked: boolean;
 }
 
 const HomeProductList = ({
-  brand,
-  title,
-  price,
-  discountRate,
-  badges,
   imageUrl,
+  brandName,
+  name,
+  saleRate,
+  price,
+  tags,
+  likeCount,
+  isLiked,
 }: HomeProductListProps) => {
+  const HeartIcon = isLiked ? HeartFilledIcon : HeartBlankedIcon;
+
   return (
     <div className="flex gap-3">
-      <Image src={imageUrl} alt={title} className="h-16 w-16 rounded-sm" />
-      <div className="flex flex-col max-w-[222px]">
-        <p className="text-caption-02-semibold my-0.5">{brand}</p>
-        <h3 className="text-body-02-regular mb-0.5 line-clamp-1">{title}</h3>
-        <div className="text-body-02-bold flex gap-1 mb-1">
-          <p className="text-red">{discountRate}%</p>
+      <Image src={imageUrl} alt={name} className="h-16 w-16 rounded-sm" />
+      <div className="flex max-w-[222px] flex-col">
+        <p className="text-caption-02-semibold my-0.5">{brandName}</p>
+        <h3 className="text-body-02-regular mb-0.5 line-clamp-1">{name}</h3>
+        <div className="text-body-02-bold mb-1 flex gap-1">
+          <p className="text-red">{saleRate}%</p>
           <p>{price.toLocaleString()}</p>
         </div>
         <div className="flex gap-[5px] overflow-hidden">
-          {badges.map((b, i) => (
+          {tags.map((label, i) => (
             <div key={i} className="shrink-0">
-              <Chip label={b} />
+              <Chip label={label} />
             </div>
           ))}
         </div>
       </div>
+      <button
+        type="button"
+        aria-label={isLiked ? '좋아요 취소' : '좋아요'}
+        aria-pressed={isLiked}
+        className="flex cursor-pointer flex-col items-center justify-center"
+      >
+        <HeartIcon className="h-5 w-5 text-gray-700" />
+        <p className="text-body-02-regular text-gray-700">
+          {new Intl.NumberFormat('en', {
+            notation: 'compact',
+            maximumFractionDigits: 0,
+          }).format(likeCount)}
+        </p>
+      </button>
     </div>
   );
 };

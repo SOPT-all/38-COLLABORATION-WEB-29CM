f6c0ae6fc84362f6eced6ed3f00eebcb627b8f44
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-05-13 15:53:45 +0900

    refactor: 목록 FOOTER_ITEMS constants로 변경

---
+++ src/shared/ui/footer/footer.constants.ts
@@ -0,0 +1,30 @@
+export const FOOTER_ITEMS = [
+  {
+    title: 'ABOUT US',
+    links: ['인재채용', '상시 할인 혜택'],
+  },
+  {
+    title: 'MY ORDER',
+    links: [
+      '주문배송',
+      '취소/교환/반품 내역',
+      '상품 리뷰 내역',
+      '증빙서류발급',
+    ],
+  },
+  {
+    title: 'MY ACCOUNT',
+    links: ['회원정보수정', '나의 멤버십 등급', '적립금 현황', '쿠폰'],
+  },
+  {
+    title: 'HELP',
+    links: [
+      '1:1 문의',
+      '입점 및 제휴 문의',
+      '상품 Q&A 내역',
+      '공지사항',
+      'FAQ',
+      '고객의 소리',
+    ],
+  },
+] as const;

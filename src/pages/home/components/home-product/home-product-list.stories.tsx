ef37ab487836f4c8531a6091f534f20bfb568262
pwc2002 <pwc2002@naver.com>
2026-05-14 01:44:06 +0900

    refactor: 수정된 컴포넌트에 맞추어 storybook 수정

--- src/pages/home/components/home-product/home-product-list.stories.tsx
+++ src/pages/home/components/home-product/home-product-list.stories.tsx
@@ -1,16 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+
 import HomeProductList from './home-product-list';
 
 const meta = {
   title: 'Home/home-product/home-product-list',
   component: HomeProductList,
   args: {
-    brand:"으음키임",
-    title:"[12차리오더,숏기장] 시그니쳐 세미 와이드 ",
-    price:93670,
-    discountRate:19,
-    badges:['쿠폰', '무료배송', '단독', '무료배송'],
-    imageUrl:"https://picsum.photos/200"
+    productId: 0,
+    imageUrl: 'https://picsum.photos/200',
+    brandName: '으음키임',
+    name: '[12차리오더,숏기장] 시그니쳐 세미 와이드 데님 팬츠 테스트 1234 가나다...',
+    saleRate: 19,
+    price: 93670,
+    tags: ['쿠폰', '무료배송', '단독', '무료배송'],
+    likeCount: 35000,
+    isLiked: false,
   },
 } satisfies Meta<typeof HomeProductList>;
 
@@ -18,6 +22,10 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const HomeProductListStory: Story = {
-  render: () => <HomeProductList {...meta.args} />,
+export const Default: Story = {};
+
+export const Liked: Story = {
+  args: {
+    isLiked: true,
+  },
 };

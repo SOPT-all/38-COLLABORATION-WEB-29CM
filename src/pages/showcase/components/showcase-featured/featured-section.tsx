b854080dc01d6a6b38d63942da976cc9e391e596
chaewon41 <chaew0m3x@gmail.com>
2026-05-15 12:58:59 +0900

    feat: featured 섹션 구현

---
+++ src/pages/showcase/components/showcase-featured/featured-section.tsx
@@ -0,0 +1,30 @@
+import ShowcaseCard from '../showcase-card/showcase-card';
+
+interface FeaturedItem {
+  showcaseId: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  startDate: string;
+  endDate: string;
+}
+
+interface FeaturedSectionProps {
+  featured: FeaturedItem[];
+}
+
+const FeaturedSection = ({ featured }: FeaturedSectionProps) => {
+  return (
+    <section className="py-16 pr-13 pl-9">
+      <ul className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+        {featured.map(({ showcaseId, ...props }) => (
+          <li key={showcaseId}>
+            <ShowcaseCard variant="big" {...props} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default FeaturedSection;

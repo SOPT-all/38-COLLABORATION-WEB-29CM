9565b28e32e59a605cf927e43927045b0210555c
Navi <masimello0001@gmail.com>
2026-05-16 21:03:35 +0900

    fix: 코드 리뷰 반영

--- src/pages/home/components/home-mega-menu/home-mega-menu.tsx
+++ src/pages/home/components/home-mega-menu/home-mega-menu.tsx
@@ -8,16 +8,17 @@ const HomeMegaMenu = ({ middleCategories }: HomeMegaMenuProps) => {
   return (
     <div className="flex">
       {middleCategories.map(({ middleCategoryId, name, subCategories }) => (
-        <div key={middleCategoryId} className="pt-[22px] pl-9 pr-[50px] pb-[22px]">
-          <span className="text-caption-01 flex items-center px-[10px] py-2">
-            {name}
-          </span>
+        <div
+          key={middleCategoryId}
+          className="pt-[22px] pr-[50px] pb-[22px] pl-9"
+        >
+          <span className="text-caption-01 px-[10px] py-2">{name}</span>
           <ul>
             {subCategories.map(({ subCategoryId, name }) => (
               <li key={subCategoryId}>
                 <button
                   type="button"
-                  className="text-body-02-regular flex items-center px-[10px] py-[5px] whitespace-nowrap"
+                  className="text-body-02-regular px-[10px] py-[5px] whitespace-nowrap"
                 >
                   {name}
                 </button>

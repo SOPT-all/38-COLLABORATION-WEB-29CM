abe24749a43439a0ca82b08ce10ba4863257bf94
Navi <masimello0001@gmail.com>
2026-05-17 02:35:31 +0900

    feat: 홈 페이지 뷰 구현

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,7 +1,19 @@
+import BannerCarousel from './components/banner-carousel/banner-carousel';
+import CategorySection from './components/home-category/category-section';
+import { CATEGORIES } from './components/home-menu/constants';
+import HomeMenu from './components/home-menu/home-menu';
+import ProductSelectionSection from './components/product-selection-section/product-selection-section';
+import { BANNER_IMAGES, SECTIONS, SHORTCUTS } from './constants';
+
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <HomeMenu categories={CATEGORIES} />
+      <BannerCarousel images={BANNER_IMAGES} />
+      <CategorySection categories={SHORTCUTS} />
+      {SECTIONS.map((section) => (
+        <ProductSelectionSection key={section.sectionId} section={section} />
+      ))}
     </div>
   );
 };

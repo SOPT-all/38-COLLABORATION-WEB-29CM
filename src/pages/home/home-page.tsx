b17e8bfe198e3eda58007ed9a76ba4eda30f699e
JS MacBook Pro <j_seokpark@naver.com>
2026-05-17 17:57:09 +0900

    feat: home 페이지에 캐러셀 api 연동

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,15 +1,18 @@
+import { useHomeCarouselsQuery } from './api/carousels';
 import BannerCarousel from './components/banner-carousel/banner-carousel';
 import CategorySection from './components/home-category/category-section';
 import { CATEGORIES } from './components/home-menu/constants';
 import HomeMenu from './components/home-menu/home-menu';
 import ProductSelectionSection from './components/product-selection-section/product-selection-section';
-import { BANNER_IMAGES, SECTIONS, SHORTCUTS } from './constants';
+import { SECTIONS, SHORTCUTS } from './constants';
 
 const HomePage = () => {
+  const { data: carousels = [] } = useHomeCarouselsQuery();
+
   return (
     <>
       <HomeMenu categories={CATEGORIES} />
-      <BannerCarousel images={BANNER_IMAGES} />
+      <BannerCarousel images={carousels} />
       <CategorySection categories={SHORTCUTS} />
       {SECTIONS.map((section) => (
         <ProductSelectionSection key={section.sectionId} section={section} />

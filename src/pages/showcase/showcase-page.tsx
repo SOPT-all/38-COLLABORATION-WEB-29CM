ef5175c11e4855910e673ded1eada7bdd4da5c48
Lee Hye Won <thankyou@sookmyung.ac.kr>
2026-05-18 15:46:11 +0900

    feat: ContentSection 스켈레톤 및 Suspense 적용

--- src/pages/showcase/showcase-page.tsx
+++ src/pages/showcase/showcase-page.tsx
@@ -1,6 +1,9 @@
 import ContentSection from './components/showcase-content/content-section';
 import FeaturedSection from './components/showcase-featured/featured-section';
 import { MOCK_SHOWCASE_RESPONSE } from './showcase-page.mock';
+import DeferredComponent from '@shared/ui/deferred-component';
+import ContentSkeleton from '@shared/ui/skeleton/content-skeleton';
+import { Suspense } from 'react';
 
 const ShowcasePage = () => {
   const { featured, sections } = MOCK_SHOWCASE_RESPONSE;
@@ -10,7 +13,16 @@ const ShowcasePage = () => {
       <FeaturedSection featured={featured} />
 
       {sections.map((section) => (
-        <ContentSection key={section.sectionId} section={section} />
+        <Suspense
+          key={section.sectionId}
+          fallback={
+            <DeferredComponent>
+              <ContentSkeleton />
+            </DeferredComponent>
+          }
+        >
+          <ContentSection section={section} />
+        </Suspense>
       ))}
     </>
   );

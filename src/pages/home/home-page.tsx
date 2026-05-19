ee2d7e09642d6ed624fa6985c4ca6282e4003797
JS MacBook Pro <j_seokpark@naver.com>
2026-05-19 18:28:07 +0900

    feat: 더보기 버튼 연결

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
+
 import { useInfiniteScroll } from '@shared/hooks/use-infinite-scroll';
 import FloatingScrollTopButton from '@shared/ui/floating-scroll-top-button/floating-scroll-top-button';
 
 import { useHomeCarouselsQuery } from './api/carousels';
 import { useHomeMainQuery } from './api/main';
 import BannerCarousel from './components/banner-carousel/banner-carousel';
+import MoreLoadButton from './components/btn-more-load/more-load-button';
 import HomeMenu from './components/home-menu/home-menu';
 import ShortcutSection from './components/home-shortcut/shortcut-section';
 import ProductSelectionSection from './components/product-selection-section/product-selection-section';
 
 const HomePage = () => {
+  const [isInfiniteScrollEnabled, setIsInfiniteScrollEnabled] = useState(false);
   const { data: carousels = [] } = useHomeCarouselsQuery();
   const {
     data: main = { sections: [], shortcuts: [] },
@@ -23,7 +27,15 @@ const HomePage = () => {
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
+    enabled: isInfiniteScrollEnabled,
   });
+
+  const shouldShowMoreButton = !isInfiniteScrollEnabled && hasNextPage;
+
+  const handleClickMore = () => {
+    setIsInfiniteScrollEnabled(true);
+    fetchNextPage();
+  };
 
   if (isLoading) {
     return null;
@@ -41,6 +53,14 @@ const HomePage = () => {
       {main.sections.map((section) => (
         <ProductSelectionSection key={section.sectionId} section={section} />
       ))}
+      {shouldShowMoreButton && (
+        <div className="relative -mt-140 flex h-144 items-end justify-center bg-[linear-gradient(180deg,rgba(255,255,255,0)_0%,#fff_64%)] pb-12">
+          <MoreLoadButton
+            onClick={handleClickMore}
+            disabled={isFetchingNextPage}
+          />
+        </div>
+      )}
       <div ref={observerTargetRef} className="h-1" />
       <FloatingScrollTopButton />
     </>

91155e893894460028e5699586b91a4c98ab3eec
JS MacBook Pro <j_seokpark@naver.com>
2026-05-21 00:29:23 +0900

    perf: 쇼케이스 featured 이미지 우선 로딩 적용

--- src/pages/showcase/components/showcase-featured/featured-section.tsx
+++ src/pages/showcase/components/showcase-featured/featured-section.tsx
@@ -11,7 +11,7 @@ const FeaturedSection = ({ featured }: FeaturedSectionProps) => {
       <ul className="grid grid-cols-1 gap-4 md:grid-cols-2">
         {featured.map(({ showcaseId, ...props }) => (
           <li key={showcaseId}>
-            <ShowcaseCard variant="big" {...props} />
+            <ShowcaseCard variant="big" priority={true} {...props} />
           </li>
         ))}
       </ul>
